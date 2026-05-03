c9baaf9fe8a6a31684eb97d5980c29fa4fecb282
Guenni <meister.guenni3@gmail.com>
2026-05-03 15:29:30 +0200

    Unnötige Backends entfernt und vorbereitet für neues GUI

--- UniversalHookX/src/hooks/hooks.cpp
+++ UniversalHookX/src/hooks/hooks.cpp
@@ -4,10 +4,6 @@
 
 #include "hooks.hpp"
 
-#include "backend/dx10/hook_directx10.hpp"
-#include "backend/dx11/hook_directx11.hpp"
-#include "backend/dx12/hook_directx12.hpp"
-#include "backend/dx9/hook_directx9.hpp"
 
 #include "backend/opengl/hook_opengl.hpp"
 #include "backend/vulkan/hook_vulkan.hpp"
@@ -90,18 +86,6 @@ namespace Hooks {
 
         RenderingBackend_t eRenderingBackend = U::GetRenderingBackend( );
         switch (eRenderingBackend) {
-            case DIRECTX9:
-                DX9::Hook(g_hWindow);
-                break;
-            case DIRECTX10:
-                DX10::Hook(g_hWindow);
-                break;
-            case DIRECTX11:
-                DX11::Hook(g_hWindow);
-                break;
-            case DIRECTX12:
-                DX12::Hook(g_hWindow);
-                break;
             case OPENGL:
                 GL::Hook(g_hWindow);
                 break;
@@ -129,17 +113,6 @@ namespace Hooks {
 
         RenderingBackend_t eRenderingBackend = U::GetRenderingBackend( );
         switch (eRenderingBackend) {
-            case DIRECTX9:
-                DX9::Unhook( );
-                break;
-            case DIRECTX10:
-                DX10::Unhook( );
-                break;
-            case DIRECTX11:
-                DX11::Unhook( );
-                break;
-            case DIRECTX12:
-                DX12::Unhook( );
                 break;
             case OPENGL:
                 GL::Unhook( );
